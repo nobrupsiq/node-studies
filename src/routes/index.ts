54b355646f32187094a0a8d503a93e15fbce99c3
bruno <bpires.siqueira@gmail.com>
2023-02-12 18:43:16 -0300

    MVC e variáveis de ambiente

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,62 +1,15 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import * as HomeController from '../controllers/homeController';
+import * as InfoController from '../controllers/infoController';
+import * as UserController from '../controllers/userController';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  let age: number = 90;
-  let showOld: boolean = false;
-
-  if (age > 50) {
-    showOld = true;
-  }
-
-  res.render('pages/home', {
-    name: 'Bruno',
-    lastName: 'Pires',
-    age: 90,
-    showOld,
-    products: [
-      { title: 'Produto X', price: 10 },
-      { title: 'Produto Y', price: 15 },
-      { title: 'Produto W', price: 20 },
-    ],
-    frasesDoDia: [],
-  });
-});
-
-router.get('/contato', (req: Request, res: Response) => {
-  res.render('pages/contato', {});
-});
-
-router.get('/sobre', (req: Request, res: Response) => {
-  res.render('pages/sobre', {});
-});
-
-router.get('/nome', (req: Request, res: Response) => {
-  let nome: string = req.query.nome as string;
-  let idade: string = req.query.idade as string;
-
-  res.render('pages/nome', {
-    nome,
-    idade,
-  });
-});
-
-router.get('/idade', (req: Request, res: Response) => {
-  let mostrarIdade: boolean = false;
-  let idade: number = 0;
-
-  if (req.query.ano) {
-    let anoNascimento: number = parseInt(req.query.ano as string);
-    let anoAtual: number = new Date().getFullYear();
-    idade = anoAtual - anoNascimento;
-    mostrarIdade = true;
-  }
-
-  res.render('pages/idade', {
-    idade,
-    mostrarIdade,
-  });
-});
+router.get('/', HomeController.home);
+router.get('/contato', InfoController.contato);
+router.get('/sobre', InfoController.sobre);
+router.get('/nome', UserController.nome);
+router.post('/idade-resultado', UserController.idadeAction);
+router.get('/idade', UserController.idadeForm);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import path from 'path'; // caminho absoluto
 import mustache from 'mustache-express';
+import dotenv from 'dotenv';
 import mainRoutes from './routes/index';
+
+dotenv.config();
 
 const server = express();
 
@@ -11,10 +14,12 @@ server.engine('mustache', mustache()); // usando o motor de renderização musta
 
 server.use(express.static(path.join(__dirname, '../public'))); // Rotas de arquivos estáticos __dirname epecifica o diretório do arquivo que eu estou executando(server.ts)
 
+server.use(express.urlencoded({ extended: true })); // Habilita para que eu vá no body da requisição e pegue os dados que estão la e ajustes esses dados para serem acessíveis dentro da minha rota. extended: true > irá dar os dados detalhados da minha rota EX: nome da variável que eu mandei, o valor dela e tudo mais.
+
 server.use(mainRoutes);
 
 server.use((req: Request, res: Response) => {
   res.status(404).send('Página não encontrada!');
 });
 
-server.listen(80);
+server.listen(process.env.PORT);
